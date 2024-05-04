0e66166e21b7881b12fc21e4dc8e45c8d12badb2
Cyyyp100 <cyprienvial2909@gmail.com>
2024-05-04 21:14:49 +0200

    Description des modifications

--- Front/src/components/Homepage.js
+++ Front/src/components/Homepage.js
@@ -18,7 +18,7 @@ function HomePage() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href=".\">accueil</a>
+        <a className="navbar-brand" href=".\">Accueil</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
