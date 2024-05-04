2eb47ac45a244601ffa02c0fce3da9107398d819
Cyyyp100 <cyprienvial2909@gmail.com>
2024-05-04 20:25:23 +0200

    Commit

--- Front/src/components/Homepage.js
+++ Front/src/components/Homepage.js
@@ -18,14 +18,14 @@ function HomePage() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href=".\CreationEvennement">Créer un Événement</a>
+        <a className="navbar-brand" href=".\">accueil</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" href=".\">Accueil</a>
+              <a className="nav-link active" href=".\CreationEvennement">CreationEvennement</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Vignerons</a>
