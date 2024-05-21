b3e382a2bac650e58be3c1b8e9bacc9023fe709d
Cyyyp100 <cyprienvial2909@gmail.com>
2024-05-21 22:56:57 +0200

    Liens vers evennements fonctionnels

--- Front/src/components/EventPage.js
+++ Front/src/components/EventPage.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';  // Ajoutez cette ligne
+
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 
 function EventPage() {
-    const query = useQuery();
-    const eventId = query.get('id');
+    const { id } = useParams();
     const [event, setEvent] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:3001/api/evenements/${eventId}`)
+        fetch(`http://localhost:3001/api/evenements/${id}`)
         .then(response => response.json())
         .then(data => setEvent(data))
         .catch(error => console.error('Error fetching event details:', error));
-    }, [eventId]);
+    }, [id]);
 
     if (!event) {
         return <div>Loading...</div>;
@@ -23,7 +24,7 @@ function EventPage() {
 
     return (
         <div>
-            <h1>{event.name}</h1>
+            <h1>{event.nom}</h1>
             <p>Date: {new Date(event.created).toLocaleDateString()}</p>
             <p>Lieu: {event.lieu}</p>
             <p>Description: {event.description}</p>
